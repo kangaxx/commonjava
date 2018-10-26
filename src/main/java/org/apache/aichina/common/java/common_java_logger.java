ca5596826aefae1ec9d557ce90f7f16ca6541dea
kangaxx <lion_kangaxx@126.com>
2018-10-26 18:40:51 +0800

    ... ...

--- src/main/java/org/apache/aichina/common/java/common_java_logger.java
+++ src/main/java/org/apache/aichina/common/java/common_java_logger.java
@@ -1,9 +1,16 @@
 import java.util.*;
 public class common_java_logger{
+  private static common_java_logger _instance = new common_java_logger();
+  private common_java_logger(){
+    try{
+    } catch(Exception e){
+      e.printStackTrace();
+    }
+  }
   
+  public static common_java_logger getInstance(){
+    return _instance;
 
-
-
-
+  }
 
 }
