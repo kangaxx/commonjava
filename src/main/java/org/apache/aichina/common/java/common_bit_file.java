d7d23f942de2de0a62d09cecc3351ecd06d748c1
kangaxx <lion_kangaxx@126.com>
2018-12-21 10:37:43 +0800

    ... ...

---
+++ src/main/java/org/apache/aichina/common/java/common_bit_file.java
@@ -0,0 +1,66 @@
+package org.apache.aichina.common.java;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.aichina.common.java.common_java;
+
+public class common_bit_file
+{
+  private RandomAccessFile _file = null;
+  public common_bit_file(String fileName, String mode){
+    try{
+      _file = new RandomAccessFile(fileName, filterMode(mode));
+    }
+    catch(Exception e){
+    }
+  }
+
+  //判断读写文件mode选项合法性，不合法则一律返回read模式
+  private String filterMode(String mode){
+    if (mode == "rw" || mode == "r" || mode == "rws" || mode == "rwd")
+      return mode;
+    else
+      return "r";
+  }
+
+  public void read(byte[] words, int offset, int size){
+    try{
+      if (_file == null)
+        throw new Exception("ERR: read bit file error ,file not initial!");
+ 
+      _file.read(words, offset, size);
+    } catch (Exception e){
+    }
+  }
+  
+  public void write(byte[] words, int offset, int size){
+    try{ 
+      if (_file == null)
+        throw new Exception("ERR: write bit file error ,file not initial!");
+
+      _file.write(words, offset, size);
+    } catch(Exception e){
+    }
+  }
+
+  public void close(){
+    try{
+      if (_file == null)
+        throw new Exception("ERR: close bit file error, file not initial!");
+      _file.close();
+    }catch (Exception e){
+    }
+  }
+
+}
---
+++ src/main/java/org/apache/aichina/common/java/common_database_file.java
@@ -0,0 +1,42 @@
+package org.apache.aichina.common.java;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.aichina.common.java.common_global_variant;
+import org.apache.aichina.common.java.common_java;
+import org.apache.aichina.common.java.common_bit_file ;
+public class common_database_file{
+  private String infoFileName = null;
+  private String indexFileName = null;
+  private String dataFileName = null;
+  public common_database_file(String dbName, String tableName){
+   
+  
+  }
+
+  public String getTableInfoFileName(String tableName){
+    return tableName + common_global_variant.GLOB_STRING_DATABASE_INFOFILE_SUFFIX; 
+
+  }
+
+  public String getTableIndexFileName(String tableName){
+    return tableName + common_global_variant.GLOB_STRING_DATABASE_INDEXFILE_SUFFIX;
+  }
+
+  public String getTableDataFileName(String tableName){
+    return tableName + common_global_variant.GLOB_STRING_DATABASE_DATAFILE_SUFFIX;
+  }
+
+
+}
