1b2d7e0b8ef97afd560230923160346980995291
kanGaXX <lion_kangaxx@126.com>
2019-01-18 16:04:04 +0800

    .....

--- src/main/java/org/apache/aichina/common/java/common_java_logger.java
+++ src/main/java/org/apache/aichina/common/java/common_java_logger.java
@@ -1,16 +1,21 @@
 import java.util.*;
 public class common_java_logger{
   private static common_java_logger _instance = new common_java_logger();
+  private String _fileName;
   private common_java_logger(){
     try{
     } catch(Exception e){
       e.printStackTrace();
     }
   }
-  
+
   public static common_java_logger getInstance(){
     return _instance;
 
+  }
+
+  public static setFileName(String name){
+    _fileName = name;
   }
 
 }
