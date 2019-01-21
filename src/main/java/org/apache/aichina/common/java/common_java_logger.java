ec2a5672697c1aa95ff54fb0d579e72b6f20b4ea
root <root@iZ2ze3pjjhxv6g0twxdd7dZ.(none)>
2019-01-21 10:39:18 +0800

    ............

--- src/main/java/org/apache/aichina/common/java/common_java_logger.java
+++ src/main/java/org/apache/aichina/common/java/common_java_logger.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class common_java_logger{
   private static common_java_logger _instance = new common_java_logger();
   private static String _fileName;
+  private static bool _initialed = false;
   private common_java_logger(){
     try{
     } catch(Exception e){
@@ -9,13 +10,29 @@ public class common_java_logger{
     }
   }
 
+  private common_java_logger(String fileName){
+    try{
+      _fileName = fileName;
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
   public static common_java_logger getInstance(){
     return _instance;
-
   }
 
   public static void setFileName(String name){
     _fileName = name;
   }
 
+  public static void initial(){
+    try {
+      common_bit_file bf = new common_bit_file(_fileName, "rw");
+      _initialed = true;
+    } catch (Exception e) {
+      _initialed = false;
+      e.printStackTrace();
+    }
+  }
 }
